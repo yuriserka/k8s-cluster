cb5dd2f9311e33f8aa0aafc2b6eb6cd65998cf35
Yuri Serka <yserka@hotmail.com>
2026-06-02 22:51:34 -0300

    feat(kafka-worker): enhance Docker Compose configuration, add Kafka service, and integrate OpenTelemetry support

--- apps/kafka-worker/kafkaworker/config/settings.py
+++ apps/kafka-worker/kafkaworker/config/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'kafkaworker.config',
     'kafkaworker.core',
     'django_apscheduler',
     'kafkaworker.containers.api.views',
